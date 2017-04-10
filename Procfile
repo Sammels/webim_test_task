80160a5caf511663e7d86fac933c2e1480f20437
glebshishkov <glebdimant@gmail.com>
2017-04-10 16:55:55 +0300

    Ready for deployment to heroku.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn webim_test_task.wsgi --log-file -
--- webim_test_task/settings.py
+++ webim_test_task/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'bv38s(5gqzkl8w%w(d5vcz97yoy$-0wi5kno!1h)l*lc394=+a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -129,3 +129,23 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+
+# Heroku settings
+if os.getcwd() == '/webim_test_task':
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(default='postgres://localhost')
+    }
+
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure().
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    # Allow all host headers.
+    ALLOWED_HOSTS = ['*']
+
+    # Static asset configuration
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
